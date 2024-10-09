cb5167f9013affdb9e07e0ebc87bb0582aa3671a
Ahmed Mahamed <pw1814mk@students.codeinstitute.net>
2024-10-09 12:31:51 +0000

    add urls

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,16 @@
-# from django.contrib import admin
-# from .models import Post, Comment
+from django.contrib import admin
+from .models import Post, Comment
 
 
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
 
-#     list_display = ('title', 'slug', 'status', 'created_on')
-#     search_fields = ['title', 'content']
-#     list_filter = ('status', 'created_on')
-#     prepopulated_fields = {'slug': ('title',)}
-#     # summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    # summernote_fields = ('content',)
 
 
-# # Register your models here.
-# admin.site.register(Comment)
+# Register your models here.
+admin.site.register(Comment)
