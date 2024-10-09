c16510b54285326f0dca3cf2cee6f076f5b5816a
Laura Read <laurajmtb@gmail.com>
2024-10-09 09:46:00 +0000

    Add userbio liurls and test view

--- gadget_gallery/urls.py
+++ gadget_gallery/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from userbio.views import userbio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('userbio/', userbio, name='userbio'),
 ]
