a2618a38eb5e41a35a3b5ec3f56f0f203567e516
Laura Read <laurajmtb@gmail.com>
2024-10-10 10:25:59 +0000

    Update blog models and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,30 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
 
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
-        return self.title
+        return f"{self.title} | {self.author}"
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    
     approved = models.BooleanField(default=False)
 
     class Meta:
